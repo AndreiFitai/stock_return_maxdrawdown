501ab0380fde2eb8960df8ce4b4291e95732dc83
Andrei Fitai <fitai.andrei@gmail.com>
2018-10-08 16:56:25 +0200

    refactored maxDrawdown to eliminate duplications

--- calculations/maxDrawdown.js
+++ calculations/maxDrawdown.js
@@ -13,14 +13,14 @@ function calcMaxDrawdown(arr) {
     let mdd = Math.round((((el[2] - el[3]) / el[2]) * 100) * 10) / 10
 
     let dateFormated = moment(el[0]).format('DD.MM.YY')
-
+    let formatedResult = `-${mdd}% (${(el[2]).toFixed(2)} on ${dateFormated} -> ${el[3].toFixed(2)} on ${dateFormated})`
     if (index < 3) {
-      result.push(`-${mdd}% (${(el[2]).toFixed(2)} on ${dateFormated} -> ${el[3].toFixed(2)} on ${dateFormated})`)
+      result.push(formatedResult)
     }
 
     if (mdd > maxMDD.mdd) {
       maxMDD.mdd = mdd
-      maxMDD.message = `\nMaximum drawdown: -${mdd}% (${(el[2]).toFixed(2)} on ${dateFormated} -> ${el[3].toFixed(2)} on ${dateFormated})`
+      maxMDD.message = `\nMaximum drawdown: ${formatedResult})`
     }
   });
   result.push(maxMDD.message)
