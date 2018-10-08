349ea74cb60b66141b365608a24c14add6559e5c
Andrei Fitai <fitai.andrei@gmail.com>
2018-10-08 17:28:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  STOCK_API: "ZbUXiHsYozMXz6yz31bd",
-  SLACK_WEBHOOK: "https://hooks.slack.com/services/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"
+  STOCK_API: "",
+  SLACK_WEBHOOK: ""
 };
