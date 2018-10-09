57db4346602eb54f8968e4de92da7e51c083ae03
Andrei Fitai <fitai.andrei@gmail.com>
2018-10-09 17:30:05 +0200

    preping for refactor

--- calculations/rateOfReturn.js
+++ calculations/rateOfReturn.js
@@ -1,16 +1,18 @@
-const moment = require('moment')
+const moment = require("moment");
 
 function calcRateOfReturn(arr) {
+  const rateOfReturn = arr[0][4] - arr[arr.length - 1][4];
 
-  let rateOfReturn = arr[0][4] - arr[arr.length - 1][4]
+  const percentRoR =
+    Math.round((rateOfReturn / arr[arr.length - 1][4]) * 100 * 10) / 10;
 
-  let percentRoR = Math.round((((rateOfReturn) / arr[arr.length - 1][4]) * 100) * 10) / 10
+  const startDate = moment(arr[arr.length - 1][0]).format("DD.MM.YY");
 
-  let startDate = moment(arr[arr.length - 1][0]).format("DD.MM.YY")
+  const endDate = moment(arr[0][0]).format("DD.MM.YY");
 
-  let endDate = moment(arr[0][0]).format("DD.MM.YY")
-
-  return `Return: ${rateOfReturn} [${percentRoR}%] (${arr[arr.length-1][4]} on ${startDate} -> ${arr[0][4]} on ${endDate})`
+  return `Return: ${rateOfReturn} [${percentRoR}%] (${
+    arr[arr.length - 1][4]
+  } on ${startDate} -> ${arr[0][4]} on ${endDate})`;
 }
 
-module.exports = calcRateOfReturn
+module.exports = calcRateOfReturn;
--- utils/slackNotification.js
+++ utils/slackNotification.js
@@ -1,20 +1,25 @@
-const SlackWebhook = require('slack-webhook')
-const config = require('../config')
-const file = require('../stocks')
+const SlackWebhook = require("slack-webhook");
+const config = require("../config");
+const file = require("../stocks");
 
 //If slack webhook is present in config initializes slack bot service and sends message
 
 let slack;
 if (config.SLACK_WEBHOOK) {
-  slack = new SlackWebhook(config.SLACK_WEBHOOK)
+  slack = new SlackWebhook(config.SLACK_WEBHOOK);
 }
 
 function sendSlackMsg(stock, start, end, message) {
   if (config.SLACK_WEBHOOK) {
-    message = [`Stock: *${stock}*. Analysis done for the period of *${start}* to *${end}*`, ...message].join('\n')
-    slack.send(message)
-    console.log('Sent to Slack !')
+    message = [
+      `Stock: *${stock}*. Analysis done for the period of *${start}* to *${end}*`,
+      ...message
+    ].join("\n");
+    slack.send(message);
+    console.log("Sent to Slack !");
   }
 }
 
-module.exports = sendSlackMsg
+module.exports = sendSlackMsg;
+
+//optional chart create and attach to message if period of time selected > 30 days
