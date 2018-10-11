e95d0c3c1439f3d91cac42d31648de9ede1fead1
Andrei Fitai <fitai.andrei@gmail.com>
2018-10-11 05:01:35 +0200

    added extra tests

--- output.js
+++ output.js
@@ -1,23 +1,28 @@
-const moment = require('moment')
-const { rateOfReturnMsg } = require('./calculations/rateOfReturn')
-const { maxDrawdownMsg } = require('./calculations/maxDrawdown')
-const sendSlackMsg = require('./utils/slackNotification')
+const moment = require("moment");
+const { rateOfReturnMsg } = require("./calculations/rateOfReturn");
+const { maxDrawdownMsg } = require("./calculations/maxDrawdown");
+const sendSlackMsg = require("./utils/slackNotification");
 
 function sendOutput(dataArray, stock, startDate, endDate) {
   if (dataArray) {
-
-    const message = ['',
-      ...dataArray.map(day => { return `${moment(day.date).format("DD.MM.YY")}: Closed at ${day.close} (${day.low} ~ ${day.high})` }),
-      '',
+    //build message for console and to pass to other notification services
+    const message = [
+      "",
+      ...dataArray.map(day => {
+        return `${moment(day.date).format("DD.MM.YY")}: Closed at ${
+          day.close
+        } (${day.low} ~ ${day.high})`;
+      }),
+      "",
       rateOfReturnMsg(dataArray),
-      maxDrawdownMsg(dataArray)]
+      maxDrawdownMsg(dataArray)
+    ];
 
-    console.log(message.join(`\n`))
+    console.log(message.join(`\n`));
 
     //Call other notification services
-    sendSlackMsg(stock, startDate, endDate, message, dataArray)
+    sendSlackMsg(stock, startDate, endDate, message, dataArray);
   }
 }
 
-module.exports = sendOutput
-
+module.exports = sendOutput;
