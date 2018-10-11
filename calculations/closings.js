58d808e21607a5caba3a42f081163019b33b713d
Andrei Fitai <fitai.andrei@gmail.com>
2018-10-11 06:04:37 +0200

    improvde styling for closings.js

--- calculations/closings.js
+++ calculations/closings.js
@@ -3,11 +3,7 @@ const moment = require("moment");
 function closingsMsg(dataArray) {
   return [
     "",
-    ...dataArray.map(day => {
-      return `${moment(day.date).format("DD.MM.YY")}: Closed at ${day.close} (${
-        day.low
-      } ~ ${day.high})`;
-    }),
+    ...dataArray.map(day => { `${moment(day.date).format("DD.MM.YY")}: Closed at ${day.close} (${day.low} ~ ${day.high})`; }),
     ""
   ].join("\n");
 }
