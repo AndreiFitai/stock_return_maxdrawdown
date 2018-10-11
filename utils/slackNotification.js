4fb79b49bd597f060b06dfd6c4d29ea167200eed
Andrei Fitai <fitai.andrei@gmail.com>
2018-10-11 05:29:26 +0200

    slack class added directly to function

--- utils/slackNotification.js
+++ utils/slackNotification.js
@@ -1,34 +1,38 @@
 const SlackWebhook = require("slack-webhook");
 const config = require("../config");
-const moment = require('moment')
+const moment = require("moment");
 
 //If slack webhook is present in config initializes slack bot service and sends message
 
-let slack;
-if (config.SLACK_WEBHOOK) {
-  slack = new SlackWebhook(config.SLACK_WEBHOOK);
-}
-
 function sendSlackMsg(stock, start, end, message, dataArray) {
   if (config.SLACK_WEBHOOK) {
-    start = moment(start).format("DD MMM YYYY")
-    end = moment(end).format("DD MMM YYYY")
+    const slack = new SlackWebhook(config.SLACK_WEBHOOK);
+
+    start = moment(start).format("DD MMM YYYY");
+    end = moment(end).format("DD MMM YYYY");
     // if selected period is bigger than 10 days, removes stock prices and replaces them with a chart for a better view
     if (dataArray.length > 10) {
-
       message = message.slice(message.indexOf("First 3 Drawdowns:") - 1);
-      const chartDates = dataArray.map(element => element.date).reverse().join("|")
-      const chartPrices = dataArray.map(element => element.close).reverse().join(",")
+      const chartDates = dataArray
+        .map(element => element.date)
+        .reverse()
+        .join("|");
+      const chartPrices = dataArray
+        .map(element => element.close)
+        .reverse()
+        .join(",");
 
       message = {
         text: [
           `Stock: *${stock}*. Analysis done for the period of *${start}* to *${end}*`,
           ...message
         ].join("\n"),
-        attachments: [{
-          pretext: "Closing prices for the selected period of time",
-          image_url: `https://image-charts.com/chart?cht=bvs&chs=900x400&chd=t:${chartPrices}&chds=a&chxt=x,y&chxl=100:|${chartDates}&chm=B,FCECF4`
-        }]
+        attachments: [
+          {
+            pretext: "Closing prices for the selected period of time",
+            image_url: `https://image-charts.com/chart?cht=bvs&chs=900x400&chd=t:${chartPrices}&chds=a&chxt=x,y&chxl=100:|${chartDates}&chm=B,FCECF4`
+          }
+        ]
       };
     } else {
       message = [
