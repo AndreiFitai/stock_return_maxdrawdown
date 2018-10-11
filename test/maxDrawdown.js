8957b668ca1788c0bc6289f130feeb2cafeafaa5
Andrei Fitai <fitai.andrei@gmail.com>
2018-10-11 05:16:06 +0200

    saving build with added tests

--- test/maxDrawdown.js
+++ test/maxDrawdown.js
@@ -1,55 +1,74 @@
-const {
-  expect
-} = require("chai");
-const maxDrawdown = require("../calculations/maxDrawdown")
+const { expect } = require("chai");
+const { calcMaxDrawdown } = require("../calculations/maxDrawdown");
 
-describe("maxDrawdown()", function () {
-  it("Should return calculated maximum drawdown over the specified period of time and for the first three days", function () {
-    const testData = [{
-        date: '2018-01-08',
-        open: 174.35,
-        high: 175.61,
-        low: 173.93,
-        close: 174.35
-      },
-      {
-        date: '2018-01-05',
-        open: 173.44,
-        high: 175.37,
-        low: 173.05,
-        close: 175
-      },
-      {
-        date: '2018-01-04',
-        open: 172.54,
-        high: 173.47,
-        low: 172.08,
-        close: 173.03
-      }
-    ]
-    expect(maxDrawdown(testData)).to.eql({
-      firstThree: [{
+const testData = [
+  {
+    date: "2018-01-08",
+    open: 174.35,
+    high: 175.61,
+    low: 173.93,
+    close: 174.35
+  },
+  {
+    date: "2018-01-05",
+    open: 173.44,
+    high: 175.37,
+    low: 173.05,
+    close: 175
+  },
+  {
+    date: "2018-01-04",
+    open: 172.54,
+    high: 173.47,
+    low: 172.08,
+    close: 173.03
+  }
+];
+
+describe("calcMaxDrawdown()", function() {
+  it("Should return an object", function() {
+    expect(calcMaxDrawdown(testData)).to.be.an("object");
+  });
+});
+
+describe("calcMaxDrawdown()", function() {
+  it("Object should contain 'firstThree' key", function() {
+    expect(calcMaxDrawdown(testData)).to.be.own.property("firstThree");
+  });
+});
+
+describe("calcMaxDrawdown()", function() {
+  it("Object should contain 'max' key", function() {
+    expect(calcMaxDrawdown(testData)).to.be.own.property("max");
+  });
+});
+
+describe("calcMaxDrawdown()", function() {
+  it("Should return calculated maximum drawdown over the specified period of time and for the first three days", function() {
+    expect(calcMaxDrawdown(testData)).to.eql({
+      firstThree: [
+        {
           mdd: 0.8,
-          date: '04.01.18',
+          date: "04.01.18",
           high: 173.47,
           low: 172.08
         },
         {
           mdd: 1.3,
-          date: '05.01.18',
+          date: "05.01.18",
           high: 175.37,
           low: 173.05
         },
         {
           mdd: 1,
-          date: '08.01.18',
+          date: "08.01.18",
           high: 175.61,
           low: 173.93
         }
       ],
       max: {
         mdd: 1.3,
-        date: '05.01.18',
+        date: "05.01.18",
         high: 175.37,
         low: 173.05
       }
--- test/rateofReturn.js
+++ test/rateofReturn.js
@@ -1,33 +1,51 @@
-const {
-  expect
-} = require("chai");
-const rateOfReturn = require("../calculations/rateOfReturn");
+const { expect } = require("chai");
+const { calcRateOfReturn } = require("../calculations/rateOfReturn");
 
-describe("rateOfReturn()", function () {
-  it("Should return calculated RoR as units and percentage", function () {
-    const testData = [{
-        date: '2018-01-08',
-        open: 174.35,
-        high: 175.61,
-        low: 173.93,
-        close: 174.35
-      },
-      {
-        date: '2018-01-05',
-        open: 173.44,
-        high: 175.37,
-        low: 173.05,
-        close: 175
-      },
-      {
-        date: '2018-01-04',
-        open: 172.54,
-        high: 173.47,
-        low: 172.08,
-        close: 173.03
-      }
-    ]
-    expect(rateOfReturn(testData)).to.eql({
+const testData = [
+  {
+    date: "2018-01-08",
+    open: 174.35,
+    high: 175.61,
+    low: 173.93,
+    close: 174.35
+  },
+  {
+    date: "2018-01-05",
+    open: 173.44,
+    high: 175.37,
+    low: 173.05,
+    close: 175
+  },
+  {
+    date: "2018-01-04",
+    open: 172.54,
+    high: 173.47,
+    low: 172.08,
+    close: 173.03
+  }
+];
+
+describe("calcRateOfReturn()", function() {
+  it("Should return an object", function() {
+    expect(calcRateOfReturn(testData)).to.be.an("object");
+  });
+});
+
+describe("calcRateOfReturn()", function() {
+  it("Object should contain 'rateOfReturn' key", function() {
+    expect(calcRateOfReturn(testData)).to.be.own.property("rateOfReturn");
+  });
+});
+
+describe("calcRateOfReturn()", function() {
+  it("Object should contain 'max' key", function() {
+    expect(calcRateOfReturn(testData)).to.be.own.property("percentRoR");
+  });
+});
+
+describe("calcRateOfReturn()", function() {
+  it("Should return calculated RoR as units and percentage", function() {
+    expect(calcRateOfReturn(testData)).to.eql({
       rateOfReturn: 1.3199999999999932,
       percentRoR: 0.8
     });
